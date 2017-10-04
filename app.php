a2f7db5424e18c1a1dbfd75e46aa3e6f7795a9bb
ligianoazzi <ldaazzi@gmail.com>
2017-10-04 20:23:51 +0200

    class three - PSR-4 | class four - PSR-1

---
+++ app.php
@@ -0,0 +1,6 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+// fornecedor/namespace/nome_de_classe
+$router = new LdaAzzi\Router\Router;
---
+++ src/Router/Router.php
@@ -0,0 +1,14 @@
+<?php
+//arquivo de classe nao se fecha php
+// vendorname normalmente aponra pra um 'src'
+// namespace composto por 2 nomes (nome do fornecedor Ldaazzi e nome do pacote Router)
+namespace LdaAzzi\Router;
+
+
+class Router 
+{
+	public function __construct()
+	{
+		echo "esta funcionando";
+	}
+}
